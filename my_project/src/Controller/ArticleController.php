b29e9f1a5d3dc3af679618bbe332fe50dac002dd
Mogus <marko@crospot.net>
2018-11-28 17:07:02 +0100

    Added navigation

--- my_project/src/Controller/ArticleController.php
+++ my_project/src/Controller/ArticleController.php
@@ -2,10 +2,19 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController{
+class ArticleController extends Controller{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
     public function index(){
-        return new Response('<html><body>Hello</body></html>');
+//        return new Response('<html><body>Hello</body></html>');
+
+        return $this->render('articles/index.html.twig');
     }
 }
 ?>
